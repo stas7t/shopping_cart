cf8e6f1ab97369d692b00ed47da280e692f5f862
Stanislav Tyshchenko <stas7t@gmail.com>
2017-10-29 14:00:10 +0200

    Add controller tests

---
+++ spec/controllers/addresses_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+module ShoppingCart
+  RSpec.describe AddressesController, type: :controller do
+    let!(:user) { FactoryGirl.create(:user) }
+    before { sign_in(user) }
+
+    describe 'GET #index' do
+      before { get :index }
+
+      it 'return a success response' do
+        expect(response.status).to eq(200)
+        expect(response).to render_template :index
+      end
+
+      it 'assign @addresses' do
+        expect(assigns(:addresses)).not_to be_nil
+      end
+    end
+
+    describe 'PATCH #update' do
+      let(:address) { FactoryGirl.attributes_for(:address) }
+      let(:addresses_aprams) { { billing: address, shipping: address } }
+      before { patch :update, params: { addresses_form: addresses_aprams } }
+
+      it 'assign @addresses' do
+        expect(assigns(:addresses)).not_to be_nil
+      end
+
+      it 'return a success response' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'render index' do
+        expect(response).to render_template :index
+      end
+    end
+  end
+end
---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+module ShoppingCart
+  RSpec.describe OrdersController, type: :controller do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:order) { FactoryGirl.create(:order, :in_queue, user_id: user.id) }
+    before { sign_in(user) }
+
+    describe 'GET #index' do
+      before { get :index }
+
+      it 'assign @orders' do
+        expect(assigns(:orders)).not_to be_nil
+      end
+
+      it 'return a success response' do
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe 'GET #show' do
+      before { get :show, params: { id: order.id } }
+
+      it 'assign @order' do
+        expect(assigns(:order)).not_to be_nil
+      end
+
+      it 'return a success response' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'render template show' do
+        expect(response).to render_template :show
+      end
+    end
+  end
+end
