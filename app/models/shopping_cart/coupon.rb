95f4c6f07a0ade65ab61786f8f0e9c071584371f
Stanislav Tyshchenko <stas7t@gmail.com>
2017-11-04 15:51:33 +0200

    Fix coupon-order relation

--- app/models/shopping_cart/coupon.rb
+++ app/models/shopping_cart/coupon.rb
@@ -1,6 +1,6 @@
 module ShoppingCart
   class Coupon < ApplicationRecord
-    belongs_to :order, optional: true, class_name: 'ShoppingCart::Order'
+    has_many :orders, dependent: :nullify
 
     before_create :activate
 
