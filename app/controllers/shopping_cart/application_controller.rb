535cc3ed442fc632952a9ad2806d26365a2e7f2e
Stanislav Tyshchenko <stas7t@gmail.com>
2017-11-04 15:51:13 +0200

    Fix engine's ApplicationController

--- app/controllers/shopping_cart/application_controller.rb
+++ app/controllers/shopping_cart/application_controller.rb
@@ -1,25 +1,5 @@
 module ShoppingCart
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
     protect_from_forgery with: :exception
-
-    helper_method :current_order
-
-    protected
-
-    def after_sign_in_path_for(resource)
-      if cookies[:from_checkout]
-        cookies.delete :from_checkout
-        main_app.checkout_path(:addresses)
-      else
-        super
-      end
-    end
-
-    private
-
-    def current_order
-      return Order.new unless session[:order_id]
-      Order.find(session[:order_id])
-    end
   end
 end
